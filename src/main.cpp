63a6084bce231bcb4e8d7ccf315b69bfd41ec4d4
robott11 <contato.robott@gmail.com>
2023-10-14 00:46:37 -0300

    feat: lua lib setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,18 @@
+#include <cstring>
 #include <iostream>
+#include <lua.hpp>
 
 int main(int argc, char** argv)
 {
-    std::cout << "Hello World!" << std::endl;
+    void* ud = nullptr;
+    lua_State* L = luaL_newstate();
+    luaL_openlibs(L);
+
+    const char* msg = "Hello World!";
+
+    int value_type = lua_getglobal(L, "print");
+    lua_pushlstring(L, msg, strlen(msg));
+    lua_call(L, 1, 1);
 
     return EXIT_SUCCESS;
 }
